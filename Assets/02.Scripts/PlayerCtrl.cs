4817f76eaa624e3f611d65f12f9e53b2c5343a00
sbjung90 <sbjung90@naver.com>
2022-02-09 22:06:27 +0900

    Initial Commit

---
+++ Assets/02.Scripts/PlayerCtrl.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCtrl : MonoBehaviour
+{
+    public GameObject plane;
+    private MeshFilter meshFilter;
+    private Animator animator;
+    public float speed = 6.0f;
+    public VariableJoystick variableJoystick;
+    private Vector3 max;
+    private Vector3 min;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        meshFilter = plane.GetComponent<MeshFilter>();
+        Bounds bounds = meshFilter.mesh.bounds;
+        Debug.Log($"bounds Size : {bounds.size}");
+        Transform planeTransform = plane.GetComponent<Transform>();
+        Debug.Log($"planeTransform scale : {planeTransform.localScale}");
+
+        max = new Vector3(planeTransform.localScale.x * bounds.size.x * 0.5f - 10.0f, 0, planeTransform.localScale.z * bounds.size.z * 0.5f - 10.0f);
+        min = max * -1f;
+        Debug.Log($"max : {max} min: {min}");
+          
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log($"Horizontal : {variableJoystick.Horizontal} Vertical : {variableJoystick.Vertical}");
+        Vector3 moveDir = Vector3.forward * variableJoystick.Vertical + Vector3.right * variableJoystick.Horizontal;
+        moveDir.y = 0f;
+
+        animator.SetFloat("Movement", moveDir.magnitude);
+
+        if (moveDir != Vector3.zero)
+        {
+            transform.rotation = Quaternion.LookRotation(moveDir);
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+
+        }
+        Debug.Log($"X : {transform.position.x} Z : {transform.position.z}");
+
+    }
+}
